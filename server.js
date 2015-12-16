22f0dada9a4f7e77572e25b3387e82c3058c728b
napeters <nicole.asha.peters@gmail.com>
2015-12-16 14:55:45 -0500

    took out reference of server variable

--- server.js
+++ server.js
@@ -14,11 +14,7 @@ db.once('open', function (callback) {
   console.log('Database Connection Established');
 });
 
-app.listen(process.env.PORT || 5000, function() {
-  const host = server.address().address;
-  const port = server.address().port;
-  console.log('express running', host, port);
-});
+app.listen(process.env.PORT || 5000);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
